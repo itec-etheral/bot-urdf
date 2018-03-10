5269b94ae826b5e2bbb2f31abcf6d05b2f24a7ab
PaulakaPaul <p.e.iusztin@gmail.com>
2018-03-10 17:26:26 +0200

    Road Visuals

--- goodbot_test.py
+++ goodbot_test.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pybullet_data
 from goodbot import Goodbot
+from Road import Road
 
 phyCl = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -8,6 +9,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 plane = p.loadURDF("plane.urdf") # load the plane
 p.setGravity(0, 0, -10) # set gravity
 p.setRealTimeSimulation(1)
+
+road = Road()
+road.create_road()
 
 robot = Goodbot("goodbot.urdf", [0, 0, 1])
 
